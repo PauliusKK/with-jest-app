31c6bc1f07ed7d7bcb1473d3ea74a9bf6117ce78
Paulius Kašiuba <17347293+PauliusKK@users.noreply.github.com>
2023-01-05 21:22:45 +0200

    Added small error handling for Calculator input and a red color in styles.

--- pages/calculator/components/Calculator.tsx
+++ pages/calculator/components/Calculator.tsx
@@ -1,5 +1,7 @@
 import { ChangeEvent, useState } from "react";
 
+import styles from "@/pages/index.module.css";
+
 const RomanNumerals = {
   M: 1000,
   CM: 900,
@@ -20,11 +22,27 @@ export const Calculator = () => {
   const [inputValue, setInputValue] = useState("");
   const [convertedValueInRomanNumerals, setConvertedValueInRomanNumerals] =
     useState("");
+  const [error, setError] = useState("");
 
   const handleInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
 
     setInputValue(value);
+    setConvertedValueInRomanNumerals("");
+
+    if (value.includes(".") || value.includes(",")) {
+      return setError("Value should not have decimals");
+    }
+
+    if (+value < 1) {
+      return setError("Value is too low, must be at least 1");
+    }
+
+    if (+value > 1000) {
+      return setError("Value is too high, must not be higher than 1000");
+    }
+
+    setError("");
     getConvertedValueInRomanNumerals(+value);
   };
 
@@ -47,6 +65,8 @@ export const Calculator = () => {
   return (
     <>
       <input value={inputValue} onChange={handleInputValueChange} />
+
+      {error && <p className={styles.redText}>{error}</p>}
 
       <p>Your value in Roman Numerals is: {convertedValueInRomanNumerals}</p>
     </>
